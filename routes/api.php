d6819d3178afda99d170dcb2a2af5e7e4e2ee17c
Sergio-S-S <fajos12@gmail.com>
2024-11-25 21:21:23 +0100

    Implementación del controlador CSV y tests

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JsonController;
 use App\Http\Controllers\HelloWorldController;
-
-
+use App\Http\Controllers\CsvController;
 
 
 
@@ -15,3 +14,11 @@ Route::post('/json', [JsonController::class, 'store']);
 Route::get('/json/{filename}', [JsonController::class, 'show']);
 Route::put('/json/{filename}', [JsonController::class, 'update']);
 Route::delete('/json/{filename}', [JsonController::class, 'destroy']);
+
+Route::prefix('csv')->group(function () {
+    Route::get('/', [CsvController::class, 'index']);
+    Route::post('/', [CsvController::class, 'store']);
+    Route::get('/{id}', [CsvController::class, 'show']);
+    Route::put('/{id}', [CsvController::class, 'update']);
+    Route::delete('/{id}', [CsvController::class, 'destroy']);
+});
